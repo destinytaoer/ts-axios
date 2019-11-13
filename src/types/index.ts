2ddcf37313925aed47c6add9ccfa7f04ca801f95
destinytaoer <1848765519@qq.com>
2019-11-13 22:33:43 +0800

    feat(src): actualize a simple ajax request
    use xhr actual a simple ajax request

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'patch'
+  | 'PATCH'
+// 请求配置对象接口
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
