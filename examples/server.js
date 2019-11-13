5e9ee0e5cf75003ada9a3fb04b42083f938ee3c2
destinytaoer <1848765519@qq.com>
2019-11-13 22:41:15 +0800

    chore(examples): add simple feature demo
    add simple feature demo

--- examples/server.js
+++ examples/server.js
@@ -27,6 +27,13 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+router.get('/simple/get', function(req, res) {
+  res.json({
+    msg: 'hello world'
+  })
+})
+app.use(router)
+
 const port = process.env.PORT || 8080
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
