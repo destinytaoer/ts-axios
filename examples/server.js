347d409f0f660a13f898e2b8992fc4a9758cd188
destinytaoer <1848765519@qq.com>
2019-11-13 22:39:05 +0800

    chore(examples): build examples compile environment
    use express + webpack to build a examples compile envioronment

---
+++ examples/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const WebpackConfig = require('./webpack.config')
+const router = express.Router()
+
+const app = express()
+const compiler = webpack(WebpackConfig)
+
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: '/__build__/',
+    stats: {
+      colors: true,
+      chunks: false
+    }
+  })
+)
+
+app.use(webpackHotMiddleware(compiler))
+
+// 开启静态服务器
+app.use(express.static(__dirname))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const port = process.env.PORT || 8080
+module.exports = app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
+})
