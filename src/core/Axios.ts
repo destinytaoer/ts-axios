a6c763b8e93ad7560f26c9a127a645265c419092
destinytaoer <1848765519@qq.com>
2019-11-16 12:58:50 +0800

    feat(main, core): actualize axios and axios.request support two params
    axios and axios.request support two parameters: url, config

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -1,7 +1,16 @@
 import { AxiosRequestConfig, AxiosPromise, Method } from '../types'
 import dispatchRequest from './dispatchRequest'
 export default class Axios {
-  request(config: AxiosRequestConfig): AxiosPromise {
+  request(url: any, config?: any): AxiosPromise {
+    // 函数的重载时, 参数都应该是 any
+    if (typeof url === 'string') {
+      if (!config) {
+        config = {}
+      }
+      config.url = url
+    } else {
+      config = url
+    }
     return dispatchRequest(config)
   }
   get(url: string, config?: AxiosRequestConfig): AxiosPromise {
