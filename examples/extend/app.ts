14a8e8f0efb39b7361a186399f80aa9de73ebb08
destinytaoer <1848765519@qq.com>
2019-11-16 12:50:31 +0800

    chore(examples): add demos to check api expand

---
+++ examples/extend/app.ts
@@ -0,0 +1,35 @@
+import axios from '../../src/index'
+
+// axios 发送请求
+axios({
+  url: '/extend/post',
+  method: 'post',
+  data: {
+    msg: 'hi'
+  }
+})
+
+// 利用 axios.request 方法发送请求, 本质上与 axios 方法一致
+axios.request({
+  url: '/extend/post',
+  method: 'post',
+  data: {
+    msg: 'hello'
+  }
+})
+
+// axios.get
+axios.get('/extend/get')
+// axios.options
+axios.options('/extend/options')
+// axios.delete
+axios.delete('/extend/delete')
+// axios.head
+axios.head('/extend/head')
+
+// axios.post
+axios.post('/extend/post', { msg: 'post' })
+// axios.put
+axios.put('/extend/put', { msg: 'put' })
+// axios.patch
+axios.patch('/extend/patch', { msg: 'patch' })
