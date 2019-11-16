75b9c10f1c253026f1f5e1f66c961a80aaebf2e1
destinytaoer <1848765519@qq.com>
2019-11-16 11:18:13 +0800

    refactor(main): to export the types
    to export the types

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,5 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
-import xhr from './xhr'
-import { buildURL } from './helpers/url'
-import { transformRequest, transformResponse } from './helpers/data'
-import { processHeaders } from './helpers/headers'
-function axios(config: AxiosRequestConfig): AxiosPromise {
-  processConfig(config)
-  return xhr(config).then(res => {
-    return transformResponseData(res)
-  })
-}
-
-function processConfig(config: AxiosRequestConfig): void {
-  config.url = transformURL(config)
-  // 先处理 headers
-  config.headers = transformHeaders(config)
-  config.data = transformData(config)
-}
-
-function transformURL(config: AxiosRequestConfig): string {
-  const { url, params } = config
-  return buildURL(url, params)
-}
-
-function transformData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-
-function transformHeaders(config: AxiosRequestConfig): any {
-  // headers 给一个默认的普通对象, 如果没有传递 headers, 默认使用 json 进行处理
-  const { headers = {}, data } = config
-  return processHeaders(headers, data)
-}
-
-function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transformResponse(res.data)
-  return res
-}
+import axios from './axios'
 
 export default axios
+// 将定义的所有接口导出
+export * from './types'
