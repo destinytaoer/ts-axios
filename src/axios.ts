57462093c5624ac6c30f847afb6e368f99f9c1a4
destinytaoer <1848765519@qq.com>
2019-11-20 19:59:29 +0800

    feat(main, types): add a feature that can create new axios instance
    1. add a AxiosStatic interface that include create method
    2. add create function that can create
    new axios instance

--- src/axios.ts
+++ src/axios.ts
@@ -1,16 +1,21 @@
-import { AxiosInstance, AxiosResponse, AxiosRequestConfig } from './types'
+import { AxiosStatic, AxiosRequestConfig } from './types'
 import Axios from './core/Axios'
 import { extend } from './helpers/util'
 import defaults from './defaults'
+import mergeConfig from './core/mergeConfig'
 
-function createInstance(config: AxiosRequestConfig): AxiosInstance {
+function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(config)
   // instance 是一个函数, 需要包含 get/post 等方法, 但是 Axios 中定义的 get 等方法需要用到 this, 所以需要 bind 这个 context, 否则 this 会是 request 函数本身
   const instance = Axios.prototype.request.bind(context)
   extend(instance, context)
-  return instance as AxiosInstance
+  return instance as AxiosStatic
 }
 
 const axios = createInstance(defaults)
+
+axios.create = function create(config: AxiosRequestConfig): AxiosStatic {
+  return createInstance(mergeConfig(defaults, config))
+}
 
 export default axios
