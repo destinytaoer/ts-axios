f2a98b7f1f9ecaeef862788a67e81054c3d25cef
destinytaoer <1848765519@qq.com>
2019-11-21 20:51:41 +0800

    chore(examples): add demos to check success status validator

--- examples/more/app.ts
+++ examples/more/app.ts
@@ -101,3 +101,25 @@ axios
   .then(res => {
     console.log(res)
   })
+
+axios
+  .get('/more/304')
+  .then(res => {
+    console.log(res)
+  })
+  .catch(e => {
+    console.log(e.message)
+  })
+
+axios
+  .get('/more/304', {
+    validateStatus(status) {
+      return status >= 200 && status < 400
+    }
+  })
+  .then(res => {
+    console.log(res)
+  })
+  .catch(e => {
+    console.log(e.message)
+  })
