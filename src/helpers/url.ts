d491a3a78a193e468130a602025c590a7f3b1c48
destinytaoer <1848765519@qq.com>
2019-11-21 20:55:13 +0800

    feat(types, helpers, core): add custom params serializer feature
    1. add paramsSerializer config
    2. add isURLSearchParams helper
    3. modify buildURL method

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -1,4 +1,4 @@
-import { isDate, isPlainObject } from './util'
+import { isDate, isPlainObject, isURLSearchParams } from './util'
 
 interface URLOrigin {
   protocol: string
@@ -24,42 +24,55 @@ function encode(val: string): string {
  * @param url
  * @param params
  */
-export function buildURL(url: string, params?: any): string {
+export function buildURL(
+  url: string,
+  params?: any,
+  paramsSerializer?: (params: any) => string
+): string {
   // 如果没有传递 params, 则不需要拼接, 直接返回即可
   if (!params) {
     return url
   }
-  const parts: string[] = []
-  Object.keys(params).forEach(key => {
-    const val = params[key]
-    // 值为 null 或者 undefined, 则忽略它
-    if (val === null || typeof val === 'undefined') {
-      return
-    }
-    // 对值为数组的处理, 如果不是数组也将值放为数组中处理
-    // 数组 params: aa: ['1', '2'] => ?aa[]=1&aa[]=2
-    let values = []
-    if (Array.isArray(val)) {
-      values = val
-      key += '[]' // 值为数组的 key 增加 []
-    } else {
-      values = [val]
-    }
-    values.forEach(val => {
-      // 对值进行类型判断
-      if (isDate(val)) {
-        val = val.toISOString()
-      } else if (isPlainObject(val)) {
-        val = JSON.stringify(val)
-      } else {
-        // 其他值不做任何处理
-      }
-      parts.push(`${encode(key)}=${encode(val)}`)
-    })
-  })
 
-  // 将所有键值对拼接起来
-  let serializedParams = parts.join('&')
+  let serializedParams
+
+  if (paramsSerializer) {
+    serializedParams = paramsSerializer(params)
+  } else if (isURLSearchParams(params)) {
+    serializedParams = params.toString()
+  } else {
+    const parts: string[] = []
+    Object.keys(params).forEach(key => {
+      const val = params[key]
+      // 值为 null 或者 undefined, 则忽略它
+      if (val === null || typeof val === 'undefined') {
+        return
+      }
+      // 对值为数组的处理, 如果不是数组也将值放为数组中处理
+      // 数组 params: aa: ['1', '2'] => ?aa[]=1&aa[]=2
+      let values = []
+      if (Array.isArray(val)) {
+        values = val
+        key += '[]' // 值为数组的 key 增加 []
+      } else {
+        values = [val]
+      }
+      values.forEach(val => {
+        // 对值进行类型判断
+        if (isDate(val)) {
+          val = val.toISOString()
+        } else if (isPlainObject(val)) {
+          val = JSON.stringify(val)
+        } else {
+          // 其他值不做任何处理
+        }
+        parts.push(`${encode(key)}=${encode(val)}`)
+      })
+    })
+
+    // 将所有键值对拼接起来
+    serializedParams = parts.join('&')
+  }
 
   if (serializedParams) {
     const markIndex = url.indexOf('#')
